6b1c1c5c1c65423182bda335b786cac125c397d4
Nico Franke <ZerNico.yt@gmail.com>
2020-03-09 18:35:30 +0100

    Initial feature section

--- pages/index.vue
+++ pages/index.vue
@@ -18,20 +18,66 @@
             />
           </div>
           <div class="col-span-2 order-last md:col-span-1 md:order-first">
-            <div class="">
+            <div>
               <h1 class="text-3xl font-bold text-green-500 pb-6">Android™, enhanced</h1>
               <div class="text-gray-700 md:px-1/12">
                 <span class="font-semibold">Potato Open Sauce Project</span>
-                <p class="pb-4">(or POSP, for short) is a buttery smooth aftermarket Android™ firmware.</p>
+                <p
+                  class="pb-4"
+                >(or POSP, for short) is a buttery smooth aftermarket Android™ firmware.</p>
                 <p>Combining newest security patches, original features and wide device support, POSP is a product created to make your life easier and breathe some fresh air into the custom ROM scene.</p>
               </div>
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-6 py-3 px-5 rounded-full">Learn more</button>
             </div>
           </div>
         </div>
       </div>
     </section>
     <!-- Feature section -->
-    <section id="feature-section" class="section">Features....</section>
+    <section id="feature-section" class="section">
+      <div class="grid grid-cols-8 items-center p-6 row-gap-6 md:row-gap-20 md:pl-1/6">
+        <div class="col-span-8 md:col-span-4">
+          <div class="inline-block mx-auto">
+            <h1
+              class="font-bold text-white text-left hidden md:block md:text-xl lg:text-2xl xl:text-3xl"
+            >
+              <p>We've included many</p>
+              <p>useful features,</p>
+              <p>to help with everyday</p>
+              <p>tasks</p>
+            </h1>
+            <h1 class="font-bold text-white text-center text-2xl sm:text-3xl md:hidden">
+              <p>We've included many useful</p>
+              <p>features,</p>
+              <p>to help with everyday tasks</p>
+            </h1>
+          </div>
+        </div>
+        <div class="col-span-8 md:col-span-4">
+          <img
+            id="hero"
+            src="../assets/img/better_notch.svg"
+            class="w-full mx-auto w-24 md:w-64 lg:w-80 xl:w-96"
+          />
+        </div>
+        <div class="col-span-8 md:col-span-3">
+          <img
+            id="hero"
+            src="../assets/img/volpanel_small.svg"
+            class="w-full mx-auto w-10/12 md:hidden"
+          />
+        </div>
+        <div class="col-span-8 md:col-span-4">
+          <img id="hero" src="../assets/img/ota.svg" class="w-full mx-auto md:w-64 lg:w-80 xl:w-96" />
+        </div>
+      </div>
+      <img
+        id="hero"
+        src="../assets/img/volpanel.svg"
+        style=""
+        class="absolute bottom-0 left-1/6 hidden md:block md:w-48 lg:w-56 xl:w-64"
+      />
+    </section>
     <!-- Team section -->
     <section id="team-section" class="section">Team....</section>
     <section id="changelog-section" class="section">Changelog....</section>
