b3126da7fb341d861d019094f5c00db31fdf1a80
Nico Franke <nico.franke01@gmail.com>
2021-03-29 13:53:11 +0200

    Fix cut off on download page

--- pages/devices.vue
+++ pages/devices.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="flex flex-col justify-center items-center h-screen">
-    <button v-for="device in devices" :key="device.name"
-        class="px-5 py-3 mt-6 font-semibold text-white rounded-full focus:outline-none bg-p-blue hover:bg-blue-700">
+  <div class="w-screen">
+    <button
+      v-for="device in devices"
+      :key="device.name"
+      class="px-5 py-3 my-6 font-semibold text-white rounded-full focus:outline-none bg-p-blue hover:bg-blue-700 block mr-auto ml-auto"
+    >
       <a :href="device.url">{{ device.name }}</a>
     </button>
-
     <button
-        class="px-5 py-3 mt-6 font-semibold text-white rounded-full focus:outline-none bg-p-gray hover:bg-gray-700">
-            <a href='https://sourceforge.net/projects/posp/files/'>Archive</a>
+      class="px-5 py-3 my-6 font-semibold text-white rounded-full focus:outline-none bg-p-gray hover:bg-gray-700 block mr-auto ml-auto"
+    >
+      <a href="https://sourceforge.net/projects/posp/files/">Archive</a>
     </button>
   </div>
 </template>
@@ -15,11 +18,15 @@
 <script>
 export default {
   async asyncData({ $axios }) {
-    const fetchedDevices = await $axios.$get('https://raw.githubusercontent.com/PotatoProject/vendor_potato/dumaloo-release/devices.json')
-    const devices = Array.from(Object.entries(fetchedDevices)).map(([name, body]) => {
-      return { url: `${body.repo}/releases/latest`, name }
-    })
-    return { devices }
+    const fetchedDevices = await $axios.$get(
+      "https://raw.githubusercontent.com/PotatoProject/vendor_potato/dumaloo-release/devices.json"
+    );
+    const devices = Array.from(Object.entries(fetchedDevices)).map(
+      ([name, body]) => {
+        return { url: `${body.repo}/releases/latest`, name };
+      }
+    );
+    return { devices };
   },
 };
 </script>
